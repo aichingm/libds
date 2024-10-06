9aeffdc03202a0e1b8f04264ffe7820061b9561d
Mario Aichinger <aichingm@gmail.com>
2024-10-07 01:54:39 +0200

    Switch from gnu indent to clang-format

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: default clean indent test compile_commands.json
+.PHONY: default clean format test compile_commands.json
 
 MAIN = bin/tests
 
@@ -12,11 +12,6 @@ LFLAGS   :=
 INCLUDES := -I.
 LIBS     :=
 
-
-INDENT_STYLE := $(shell cat indent.pro | sed -e 's/^\s*#.*$$//g' | tr '\n' ' ' | sed -e 's/\s\s*/ /g' -e 's/^ //' -e 's/\s$$//')
-TYPE_DEFS := $(shell ctags -x --c-types=t $(HDRS) $(SRCS) | awk '{print $$1}' | tr '\n ' ' ' | sed -e 's/^/ /' -e 's/ $$//' -e 's/ / -T /g')A
-INDENT_CMD := VERSION_CONTROL=none indent
-
 default: $(MAIN)
 
 $(MAIN): $(OBJS)
@@ -26,15 +21,8 @@ $(MAIN): $(OBJS)
 tests/all-tests.o: tests/all-tests.c $(HDRS)
 	$(CC) $(CFLAGS) $(INCLUDES) -c tests/all-tests.c  -o tests/all-tests.o
 
-indent: $(SRCS) $(HDRS)
-	@for f in $(HDRS); do \
-		$(INDENT_CMD) $(INDENT_STYLE) $(TYPE_DEFS) $$f; \
-		echo "" >> $$f;\
-	done
-	@for f in $(SRCS); do \
-		$(INDENT_CMD) $(INDENT_STYLE) $(TYPE_DEFS) $$f; \
-		echo "" >> $$f;\
-	done
+format: $(SRCS) $(INCLS)
+	find src/ -not -path "*/acutest.h" -a -iname '*.h' -o -iname '*.c' | xargs clang-format -style=file -i
 
 clean:
 	rm -rf $(MAIN)
--- src/list.h
+++ src/list.h
@@ -50,11 +50,15 @@ void main() {
 
 Copyright (C) 2024 Mario Aichinger <aichingm@gmail.com>
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, version 3.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
 
-You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+<https://www.gnu.org/licenses/>.
 
 */
 
@@ -68,19 +72,24 @@ You should have received a copy of the GNU Affero General Public License along w
 #define LIST_LENGTH_TYPE unsigned int
 
 // Remove an item from the end of the list and return a pointer to the struct holding the item.
-#define LIST_POP_s(type, list, property_name) ((list)->last != NULL ? LISTITEM_AS_s(type, list_pop(list), property_name): NULL)
+#define LIST_POP_s(type, list, property_name) \
+    ((list)->last != NULL ? LISTITEM_AS_s(type, list_pop(list), property_name) : NULL)
 
-// Remove an item from the end of the list and return a pointer to the struct holding the item using the default item property name.
+// Remove an item from the end of the list and return a pointer to the struct holding the item using the default item
+// property name.
 #define LIST_POP(type, list) LIST_POP_s(type, list, default_list_item_name)
 
 // Remove an item from the beginning of the list and return a pointer to the struct holding the item.
-#define LIST_SHIFT_s(type, list, property_name) ((list)->first != NULL ? LISTITEM_AS_s(type, list_shift(list), property_name) : NULL)
+#define LIST_SHIFT_s(type, list, property_name) \
+    ((list)->first != NULL ? LISTITEM_AS_s(type, list_shift(list), property_name) : NULL)
 
-// Remove an item from the beginning of the list and return a pointer to the struct holding the item using the default item property name.
+// Remove an item from the beginning of the list and return a pointer to the struct holding the item using the default
+// item property name.
 #define LIST_SHIFT(type, list) LIST_SHIFT_s(type, list, default_list_item_name)
 
 // Get the nth item list and return a pointer to the struct holding the item.
-#define LIST_GET_s(type, list, index, property_name) ((list_length(list) > index && index >= 0) ? LISTITEM_AS_s(type, list_get(list, index), property_name) : NULL)
+#define LIST_GET_s(type, list, index, property_name) \
+    ((list_length(list) > index && index >= 0) ? LISTITEM_AS_s(type, list_get(list, index), property_name) : NULL)
 
 // Get the nth item list and return a pointer to the struct holding the item using the default item property name.
 #define LIST_GET(type, list, index) LIST_GET_s(type, list, index, default_list_item_name)
@@ -108,7 +117,7 @@ You should have received a copy of the GNU Affero General Public License along w
 /**
  * Convert a listitem_t to a pointer to its holding struct using the offset of the given property's name.
  */
-#define LISTITEM_AS_s(type, ptr, property_name) ((type*)(((char*) ptr) - ((char*) offsetof(type, property_name))))
+#define LISTITEM_AS_s(type, ptr, property_name) ((type*)(((char*)ptr) - ((char*)offsetof(type, property_name))))
 
 /**
  * Convert a listitem_t to a pointer to its holding struct using the offset of the default property name.
@@ -140,12 +149,13 @@ typedef struct {
 /**
  * Iterate over the items of a list.
  */
-#define LIST_ITER(iter_name, begin) for(listitem_t* iter_name = begin; iter_name != NULL; iter_name = iter_name->next)
+#define LIST_ITER(iter_name, begin) for (listitem_t* iter_name = begin; iter_name != NULL; iter_name = iter_name->next)
 
 /**
  * Iterate in reverse order over the items of a list.
  */
-#define LIST_ITER_REVERSE(iter_name, begin) for(listitem_t* iter_name = begin; iter_name != NULL; iter_name = iter_name->prev)
+#define LIST_ITER_REVERSE(iter_name, begin) \
+    for (listitem_t* iter_name = begin; iter_name != NULL; iter_name = iter_name->prev)
 
 /**
  * Initialize a list.
@@ -195,12 +205,12 @@ bool list_contains(list_t* l, listitem_t* i);
 /**
  * Iterate over the items in the list by calling the iter function with every item. Runtime O(n)
  */
-void list_foreach(list_t* l, void (*iter)(listitem_t* , void* ), void* userdata);
+void list_foreach(list_t* l, void (*iter)(listitem_t*, void*), void* userdata);
 
 /**
  * Iterate, in reverse order, over the items in the list by calling the iter function with every item. Runtime O(n).
  */
-void list_foreach_revere(list_t* l, void (*iter)(listitem_t* , void* ), void* userdata);
+void list_foreach_revere(list_t* l, void (*iter)(listitem_t*, void*), void* userdata);
 
 /**
  * Write all item pointers contained in the list to a caller managed array. Runtime O(n).
@@ -233,7 +243,6 @@ bool listitem_in_list(listitem_t* i);
 #include <assert.h>
 
 void list_init(list_t* l) {
-
     assert(l != NULL);
 
     l->length = 0;
@@ -242,14 +251,12 @@ void list_init(list_t* l) {
 }
 
 LIST_LENGTH_TYPE list_length(list_t* l) {
-
     assert(l != NULL);
 
     return l->length;
 }
 
 void list_push(list_t* l, listitem_t* i) {
-
     assert(l != NULL);
     assert(!listitem_in_list(i));
 
@@ -267,7 +274,6 @@ void list_push(list_t* l, listitem_t* i) {
 }
 
 listitem_t* list_pop(list_t* l) {
-
     assert(l != NULL);
 
     if (l->length == 0) {
@@ -296,7 +302,6 @@ listitem_t* list_pop(list_t* l) {
 }
 
 void list_unshift(list_t* l, listitem_t* i) {
-
     assert(l != NULL);
     assert(!listitem_in_list(i));
 
@@ -314,7 +319,6 @@ void list_unshift(list_t* l, listitem_t* i) {
 }
 
 listitem_t* list_shift(list_t* l) {
-
     assert(l != NULL);
 
     if (l->length == 0) {
@@ -343,7 +347,6 @@ listitem_t* list_shift(list_t* l) {
 }
 
 listitem_t* list_get(list_t* l, LIST_LENGTH_TYPE index) {
-
     assert(l != NULL);
 
     if (index >= list_length(l)) {
@@ -361,7 +364,6 @@ listitem_t* list_get(list_t* l, LIST_LENGTH_TYPE index) {
 }
 
 listitem_t* list_remove_index(list_t* l, LIST_LENGTH_TYPE index) {
-
     assert(l != NULL);
 
     listitem_t* i = list_get(l, index);
@@ -373,46 +375,32 @@ listitem_t* list_remove_index(list_t* l, LIST_LENGTH_TYPE index) {
     return i;
 }
 
-bool list_contains(list_t* l, listitem_t* i) {
-    return i->list_ptr == l;
-}
+bool list_contains(list_t* l, listitem_t* i) { return i->list_ptr == l; }
 
-void list_foreach(list_t* l, void (*iter)(listitem_t* , void* ), void* userdata) {
-
+void list_foreach(list_t* l, void (*iter)(listitem_t*, void*), void* userdata) {
     assert(l != NULL);
 
-    LIST_ITER(elem, l->first) {
-        iter(elem, userdata);
-    }
+    LIST_ITER(elem, l->first) { iter(elem, userdata); }
 }
 
-void list_foreach_reverse(list_t* l, void (*iter)(listitem_t* , void* ), void* userdata) {
-
+void list_foreach_reverse(list_t* l, void (*iter)(listitem_t*, void*), void* userdata) {
     assert(l != NULL);
 
-    LIST_ITER_REVERSE(elem, l->last) {
-        iter(elem, userdata);
-    }
+    LIST_ITER_REVERSE(elem, l->last) { iter(elem, userdata); }
 }
 
 void list_to_array(list_t* l, listitem_t** array) {
-
     assert(l != NULL);
 
     LIST_LENGTH_TYPE i = 0;
-    LIST_ITER(elem, l->first) {
-        array[i++] = elem;
-    }
+    LIST_ITER(elem, l->first) { array[i++] = elem; }
 }
 
 void list_data_to_array(list_t* l, void** array, size_t offset) {
-
     assert(l != NULL);
 
     LIST_LENGTH_TYPE i = 0;
-    LIST_ITER(elem, l->first) {
-        array[i++] = (void *)(((char *)elem) - ((char *)offset));
-    }
+    LIST_ITER(elem, l->first) { array[i++] = (void*)(((char*)elem) - ((char*)offset)); }
 }
 
 void listitem_init(listitem_t* i) {
@@ -421,12 +409,9 @@ void listitem_init(listitem_t* i) {
     i->next = NULL;
 }
 
-bool listitem_in_list(listitem_t* i) {
-    return i->list_ptr != NULL;
-}
+bool listitem_in_list(listitem_t* i) { return i->list_ptr != NULL; }
 
 void listitem_unlink(listitem_t* i) {
-
     assert(listitem_in_list(i));
 
     list_t* l = i->list_ptr;
@@ -452,7 +437,6 @@ void listitem_unlink(listitem_t* i) {
     i->prev = NULL;
     i->next = NULL;
     i->list_ptr = NULL;
-
 }
 
 #endif
